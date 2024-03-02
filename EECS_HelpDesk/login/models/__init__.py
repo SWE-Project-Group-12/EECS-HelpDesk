b30121f54dc7fe87f5c8726c814769161a2a0e7c
mosElsh <moselsh1@gmail.com>
2024-03-02 17:09:04 +0000

    Fixed Typo

--- EECS_HelpDesk/login/models/__init__.py
+++ EECS_HelpDesk/login/models/__init__.py
@@ -2,3 +2,4 @@ from .ecHandlerModel import ECHandler
 from .userModel import User
 from .studentModel import Student
 from .technicalFaultHandlerModel import TechnicalFaultHandler
+from .admin import Admin
