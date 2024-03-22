b6681544eb292978031a6bf6fdcaa8699447a9c8
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-03-22 13:00:43 +0000

    getUserType added
    getUserType helper function that takes username and returns the user type

---
+++ EECS_HelpDesk/ticketManagement/views/getUserType.py
@@ -0,0 +1,21 @@
+from django.apps import apps
+
+def getUserType(username):
+    Student = apps.get_model("login", "Student")
+    Admin = apps.get_model("login", "Admin")
+    ECHandler = apps.get_model("login", "ECHandler")
+    TechnicalFaultHandler = apps.get_model("login", "TechnicalFaultHandler")
+
+    if len(Student.objects.filter(pk=username)) == 1:
+        return Student.__name__
+
+    elif len(Admin.objects.filter(pk=username)) == 1:
+        return Admin.__name__
+
+    elif len(ECHandler.objects.filter(pk=username)) == 1:
+        return ECHandler.__name__
+
+    elif len(TechnicalFaultHandler.objects.filter(pk=username)) == 1:
+        return TechnicalFaultHandler.__name__
+
+    return None
