328a4867a00146c13d585b839c07af4085183fd5
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-03-26 11:45:30 +0000

    findPersonalTickets updated
    Redudant logic removed

--- EECS_HelpDesk/ticketManagement/views/findPersonalTickets.py
+++ EECS_HelpDesk/ticketManagement/views/findPersonalTickets.py
@@ -26,61 +26,7 @@ from django.http import HttpResponseRedirect
 class FindPersonalTickets(ListView):
     template_name = 'findPersonalTickets.html'
     context_object_name = 'tickets'
-
-    # def dispatch(self, request, *args, **kwargs):
-    #     """
-    #     Override the dispatch method to check if the user is authenticated and of type Student.
-    #     """
-    #     # Check if the user is authenticated.
-    #     if request.session.get("user") is None:
-    #         return redirect('/login')
-
-    #     # Check if the user is of type Student.
-    #     if getUserType(request.session.get("user")) != 'Student':
-    #         return redirect('/login')
-
-    #     # Get the username from the URL.
-    #     username = kwargs['username']
-
-    #     # Check if the user is requesting their own tickets.
-    #     if username != request.session.get("user"):
-    #         return redirect('/login')
-
-    #     return super().dispatch(request, *args, **kwargs)
-
-    # def get_queryset(self):
-    #     """
-    #     Override the get_queryset method to filter the tickets based on the user's permissions.
-    #     """
-    #     # Get all tickets from the database.
-    #     queryset = super().get_queryset()
-    #     if getUserType(self.request.session.get("user")) != 'Student':
-    #         queryset = queryset.filter(pk=self.request.session.get("user"))
-    #     return queryset
-
-    # # def get_context_data(self, **kwargs):
-    # #     """
-    # #     Override the get_context_data method to add the username variable to the context.
-    # #     """
-    # #     context = super().get_context_data(**kwargs)
-    # #     context['username'] = self.request.session.get("user")
-    # #     return context
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['username'] = self.request.session.get("user")
-
-    #     context['tickets'] = [
-    #         {
-    #             'title': ticket.title,
-    #             'description': ticket.description,
-    #             'status': ticket.status,
-    #             'dateCreated': ticket.dateCreated,
-    #         } for ticket in context['tickets']
-    #     ]
-
-    #     return context
-
+    
     def get(self, request, username):
 
         if request.session.get("user") is None:
