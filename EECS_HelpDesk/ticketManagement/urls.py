b5f09fb5d5d86f039a8962afca6806312e53864c
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-03-27 12:46:57 +0000

    url names updated

--- EECS_HelpDesk/ticketManagement/urls.py
+++ EECS_HelpDesk/ticketManagement/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("createEC/<str:username>", views.createEC.as_view(), name="Create Ticket"),
-    path("createTechnicalFault/<str:username>", views.createTechnicalFault.as_view(), name="Create Technical Fault"),
+    path("createEC/<str:username>", views.createEC.as_view(), name="CreateEC"),
+    path("createTechnicalFault/<str:username>", views.createTechnicalFault.as_view(), name="CreateTechnicalFault"),
     path("deleteEC/<str:username>/<int:ticketID>", views.DeleteECView.as_view(), name="Delete EC"),
     path("deleteTechnicalFault/<str:username>/<int:ticketID>", views.DeleteTechnicalFaultView.as_view(), name="Delete Technical Fault"),
-    path("findPersonalTickets/<str:username>", views.FindPersonalTickets.as_view(), name="Find Personal Tickets"),
-    path("listAllECs", views.ListAllECs.as_view(), name="List All ECs"),
-    path("listAllTechnicalFaults", views.ListAllTechnicalFaults.as_view(), name="List All Technical Faults"),
-    path("manageTechnicalFault/<int:ticketID>", views.manageTechnicalFault.as_view(), name="Manage Technical Fault"),
-    path("manageEC/<int:ticketID>", views.manageEC.as_view(), name="Manage EC")
+    path("findPersonalTickets/<str:username>", views.FindPersonalTickets.as_view(), name="FindPersonalTickets"),
+    path("listAllECs", views.ListAllECs.as_view(), name="ListECs"),
+    path("listAllTechnicalFaults", views.ListAllTechnicalFaults.as_view(), name="ListTechnicalFaults"),
+    path("manageTechnicalFault/<int:ticketID>", views.manageTechnicalFault.as_view(), name="ManageTechnicalFault"),
+    path("manageEC/<int:ticketID>", views.manageEC.as_view(), name="ManageEC")
 ]
