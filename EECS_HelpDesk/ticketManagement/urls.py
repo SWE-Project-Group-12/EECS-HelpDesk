c7fe414fb810b49c4b93d1709a01c5491789a945
MosElsh <moselsh1@gmail.com>
2024-04-03 16:47:10 +0100

    Updated Form URL Access In Templates

--- EECS_HelpDesk/ticketManagement/urls.py
+++ EECS_HelpDesk/ticketManagement/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("createEC/<str:username>", views.createEC.as_view(), name="CreateEC"),
     path("createTechnicalFault/<str:username>", views.createTechnicalFault.as_view(), name="CreateTechnicalFault"),
-    path("deleteEC/<str:username>/<int:ticketID>", views.DeleteECView.as_view(), name="Delete EC"),
-    path("deleteTechnicalFault/<str:username>/<int:ticketID>", views.DeleteTechnicalFaultView.as_view(), name="Delete Technical Fault"),
+    path("deleteEC/<str:username>/<int:ticketID>", views.DeleteECView.as_view(), name="DeleteEC"),
+    path("deleteTechnicalFault/<str:username>/<int:ticketID>", views.DeleteTechnicalFaultView.as_view(), name="DeleteTechnicalFault"),
     path("findPersonalTickets/<str:username>", views.FindPersonalTickets.as_view(), name="FindPersonalTickets"),
     path("listAllECs", views.ListAllECs.as_view(), name="ListECs"),
     path("listAllTechnicalFaults", views.ListAllTechnicalFaults.as_view(), name="ListTechnicalFaults"),
