58ea46ee119c4e878cff416ba0611f847105c660
MosElsh <moselsh1@gmail.com>
2024-04-07 06:50:11 +0100

    Create Ticket Forms Validation Added

--- EECS_HelpDesk/ticketManagement/models/technicalFault.py
+++ EECS_HelpDesk/ticketManagement/models/technicalFault.py
@@ -1,24 +1,24 @@
 from django.db import models
 from .ticket import Ticket
 
+ITL = "ITL"
+ITS = "ITS"
+Library = "Library"
+QMplus = "QMplus"
+Mysis = "Mysis"
+Other = "Other"
+
+LOCATION_CHOICES = {
+    "ITL": "ITL",
+    'ITS': "ITS",
+    'Library': "Library",
+    'QMplus': "QMplus",
+    'Mysis': "Mysis",
+    'Other': "Other",
+}
 
 class TechnicalFault(Ticket):
     # Standard Model. Inherits from the Model Ticket.
     # Used to store Technical Faults.
-    ITL = "ITL"
-    ITS = "ITS"
-    Library = "Library"
-    QMplus = "QMplus"
-    Mysis = "Mysis"
-    Other = "Other"
-
-    LOCATION_CHOICES = {
-        ITL: "ITL",
-        ITS: "ITS",
-        Library: "Library",
-        QMplus: "QMplus",
-        Mysis: "Mysis",
-        Other: "Other",
-    }
 
     location = models.CharField(max_length=10, choices=LOCATION_CHOICES)
